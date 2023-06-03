c6ee8ef7aff36b1aec7c8d951b2d70046bf41a8e
blackmount22 <57103993+blackmount22@users.noreply.github.com>
2023-06-03 09:10:48 +0900

    Create Solution.java

---
+++ Algorithm/Level1/카드 뭉치/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        
+        int aIdx = 0;
+		int bIdx = 0;
+		
+		for(int i=0;i<goal.length;i++) {	
+			if(aIdx < cards1.length && cards1[aIdx].equals(goal[i])) {
+				aIdx++;
+			} else if(bIdx < cards2.length && cards2[bIdx].equals(goal[i])) {
+				bIdx++;
+			} 
+		}
+		
+		if (aIdx + bIdx < goal.length) answer = "No";
+        
+        return answer;
+    }
+}
