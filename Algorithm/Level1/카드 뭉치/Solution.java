e87e1f5a3fa01e89579beaaaa4b745954c228d11
blackmount22 <57103993+blackmount22@users.noreply.github.com>
2023-06-03 09:11:03 +0900

    Update Solution.java

--- Algorithm/Level1/카드 뭉치/Solution.java
+++ Algorithm/Level1/카드 뭉치/Solution.java
@@ -3,17 +3,17 @@ class Solution {
         String answer = "Yes";
         
         int aIdx = 0;
-		int bIdx = 0;
-		
-		for(int i=0;i<goal.length;i++) {	
-			if(aIdx < cards1.length && cards1[aIdx].equals(goal[i])) {
-				aIdx++;
-			} else if(bIdx < cards2.length && cards2[bIdx].equals(goal[i])) {
-				bIdx++;
-			} 
-		}
-		
-		if (aIdx + bIdx < goal.length) answer = "No";
+	int bIdx = 0;
+
+	for(int i=0;i<goal.length;i++) {	
+		if(aIdx < cards1.length && cards1[aIdx].equals(goal[i])) {
+			aIdx++;
+		} else if(bIdx < cards2.length && cards2[bIdx].equals(goal[i])) {
+			bIdx++;
+		} 
+	}
+
+	if (aIdx + bIdx < goal.length) answer = "No";
         
         return answer;
     }
