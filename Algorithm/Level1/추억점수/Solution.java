d37d970026df3e59cb4f719bbe0adb2a4e06c28b
blackmount22 <57103993+blackmount22@users.noreply.github.com>
2023-08-11 10:31:02 +0900

    Update Solution.java

--- Algorithm/Level1/추억점수/Solution.java
+++ Algorithm/Level1/추억점수/Solution.java
@@ -4,25 +4,25 @@ class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         ArrayList<Integer> ar = new ArrayList<>();
 		
-		for(int i=0;i<photo.length;i++) {
-			int score = 0;
-			
-			for(int j=0;j<photo[i].length;j++) {
-				for(int k=0;k<name.length;k++) {
-					if(name[k].equals(photo[i][j])) {	
-						score += yearning[k];
-					}
+	for(int i=0;i<photo.length;i++) {
+		int score = 0;
+		
+		for(int j=0;j<photo[i].length;j++) {
+			for(int k=0;k<name.length;k++) {
+				if(name[k].equals(photo[i][j])) {	
+					score += yearning[k];
 				}
 			}
-			
-			ar.add(score);
-		}
-		
-		int[] answer = new int[ar.size()];
-		
-		for(int i=0;i<ar.size();i++) {
-			answer[i] = ar.get(i).intValue();
-		};
+		}	
+		ar.add(score);
+	}
+	
+	int[] answer = new int[ar.size()];
+	
+	for(int i=0;i<ar.size();i++) {
+		answer[i] = ar.get(i).intValue();
+	};
+	    
         return answer;
     }
 }
